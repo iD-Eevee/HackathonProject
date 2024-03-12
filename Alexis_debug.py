1ce4fa2fdf14a60d410449bee07fdd9b7a69e9eb
Eevee <57205370+iD-Eevee@users.noreply.github.com>
2024-03-12 18:49:46 -0400

    Battle Start
    Added battle start section and placeholder variables to the main.py file. Ready to add classes.

--- Alexis_debug.py
+++ Alexis_debug.py
@@ -1,1 +1,13 @@
 # Alexis A [Team Dungeon Masters]
+
+# Player variables (replace with classes when that code's done)
+player1_name = "Warbler"
+player2_name = "Melinda"
+player1_health = 100
+player2_health = 100
+
+# Battle Arena Start
+print(player1_name + " vs. " + player2_name)
+print(str(player1_health) + " vs. " + str(player2_health))
+
+# Add While Loop next to check if they're alive (once class code is ready)
